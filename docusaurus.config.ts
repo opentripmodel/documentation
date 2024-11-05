46f1479e15c14d51b6e55b1e792d7665d952a0f3
Hein Kolk <hein.kolk@tno.nl>
2024-11-05 17:56:07 +0100

    Changed config file to show correct URL.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://opentripmodel.github.io/",
+  url: "https://opentripmodel.github.io/documentation/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/documentation/",
